731cdf0a5c9d6990fa6706b85ea161a4b722b824
amandakicke <amandakicke@gmail.com>
2022-03-18 19:38:02 -0300

    Exercicios do Curso em Video

---
+++ javascript/aula.js
@@ -0,0 +1,11 @@
+var time = 9;
+
+if(time > 6 && time < 12) {
+    console.log('bom dia');
+}
+else if (time > 12 && time < 18) {
+    console.log ('boa tarde');
+}
+else {
+    console.log('boa noite')
+}
