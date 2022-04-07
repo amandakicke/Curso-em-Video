aba5625beb7ee5049a517185289bdd31b53fc75a
amandakicke <amandakicke@gmail.com>
2022-04-07 10:46:10 -0300

    commit

---
+++ JAVA/aula01cv/src/aula01cv/Aula01cv.java
@@ -0,0 +1,18 @@
+
+package aula01cv;
+
+/**
+ *
+ * @author Amanda2
+ */
+public class Aula01cv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // TODO code application logic here
+    }
+    
+}
