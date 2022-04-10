498f7f023d295ee82b86c467a40624ba171d8197
amandakicke <amandakicke@gmail.com>
2022-04-10 10:17:37 -0300

    commit

---
+++ POO Java/Aula06/src/aula06/Aula06.java
@@ -0,0 +1,17 @@
+
+package aula06;
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.fecharMenu();
+        c.maisVolume();
+        c.play();        
+        c.pause();
+        c.desligar();
+        c.maisVolume();
+        
+    }
+    
+}
